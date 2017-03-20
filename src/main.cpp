1955e020841ecf28b8efc675e6bf80cc70d5880b
Radek Vit <radekvitr@gmail.com>
2017-03-21 00:18:03 +0100

    now running Translation in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <reon_lexical_analyzer.h>
+#include <reon_translation_grammar.h>
 #include <fstream>
 #include <functional>
 #include <iostream>
 using std::cout;
 
 int main() {
-  std::ifstream s("in");
-  ReonLexer rl;
-  std::function<Symbol(std::istream &)> fp;
-  fp = rl;
-  Token t(Token::EOI());
-  LexicalAnalyzer la{s, rl};
-  while ((t = la.get_token()) != Token::EOI()) {
-    cout << t.name() << "." << t.attribute() << "\n";
-  }
+  std::ifstream in("in");
+  std::ofstream out("out");
+  ReonLexer reonLexer;
+  LexicalAnalyzer la{reonLexer};
+  OutputGenerator og;
+
+  LLTranslationControl tc{};
+
+  Translation t{la, tc, reonGrammar, og};
+  t.run(in, out);
+  
   return 0;
 }
